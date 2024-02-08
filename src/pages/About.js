53241fc079b50329a273add10beb60d5d43eea05
berry <bairan828@gmail.com>
2024-02-07 22:13:57 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+
+
+function About(){
+    return(
+        <div>
+        <h2>About</h2>
+        <p>This is the about us seection </p>
+        </div>
+    )
+}
+export default About;
---
+++ src/pages/Properties.js
@@ -0,0 +1,9 @@
+function Properties(){
+    return(
+        <div>
+        <h2>Properties</h2>
+        <p>This is the Properties page</p>
+        </div>
+    )
+}
+export default Properties
